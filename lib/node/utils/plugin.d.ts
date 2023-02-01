7eb730329f1506c26ecd086894c6e8b036e36312
✨NeserCode <nesercode@gmail.com>
2023-02-01 21:18:47 +0800

    clear for updated version

--- lib/node/utils/plugin.d.ts
+++
@@ -1,6 +0,0 @@
-import type { App } from "@vuepress/core"
-import type { GitPluginOptions } from "@vuepress/plugin-git"
-
-export function useGitPlugin(app: App, options: GitPluginOptions): void
-
-export function removeGitPlugin(app: App): void
