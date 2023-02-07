660b55c05d864b0528a270426b0a57b1e07ca8f9
✨NeserCode <nesercode@gmail.com>
2023-02-07 18:47:50 +0800

    add comment plugin

---
+++ docs/.vuepress/theme/lib/client/components/Comment.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import Giscus from '@giscus/vue'
+
+defineProps<{
+  repo: `${string}/${string}`,
+  repoId: string,
+  category:
+}>()
+</script>
+
+<template>
+	<div class="comment" id="giscus-comment">
+		<Giscus
+			repo=""
+			repo-id=""
+			category=""
+			category-id=""
+			mapping=""
+			strict=""
+			reactions-enabled=""
+			emit-metadata=""
+			input-position=""
+			theme=""
+			lang=""
+			loading=""
+			crossorigin="anonymous"
+		/>
+	</div>
+</template>
